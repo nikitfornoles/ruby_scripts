38199bf7f703fc5108d7a543563c6640f61ea7b7
nikitfornoles <cfornoles@gbox.adnu.edu.ph>
2017-10-28 12:58:17 +0800

    added color.rb and color screenshot.png

---
+++ color.rb
@@ -0,0 +1,39 @@
+#!/usr/bin/ruby
+
+printf "\033[0mAll attributes off\033[0m\n"
+printf "\033[1mBold\033[0m\n"
+printf "\033[4mUnderline\033[0m\n"
+printf "\033[5mBlink\033[0m\n"
+printf "\033[8mHide\033[0m\n"
+printf "\033[30mBlack\033[0m\n"
+
+printf "\033[31mRed(bold)\033[0m\n"
+printf "\033[31m\033[1mRed\n\033[0m"
+
+printf "\033[32mGreen\033[0m\n"
+printf "\033[32m\033[1mGreen(bold)\033[0m\n"
+
+printf "\033[33mYellow\033[0m\n"
+printf "\033[33m\033[1mYellow(bold)\033[0m\n"
+
+printf "\033[34mBlue\033[0m\n"
+printf "\033[34m\033[1mBlue(bold)\033[0m\n"
+
+printf "\033[35mMagenta\033[0m\n"
+printf "\033[35m\033[1mMagenta(bold)\033[0m\n"
+
+printf "\033[36mCyan\033[0m\n"
+printf "\033[36m\033[1mCyan(bold)\033[0m\n"
+
+printf "\033[37mWhite\033[0m\n"
+printf "\033[37m\033[1mWhite(bold)\033[0m\n"
+
+printf "\033[40m\033[37mBlack Background\033[0m\n"
+printf "\033[41mRed Background\033[0m\n"
+printf "\033[42mGreen Background\033[0m\n"
+printf "\033[43mYellow Background\033[0m\n"
+printf "\033[44mBlue Background\033[0m\n"
+printf "\033[45mMagenta Background\033[0m\n"
+printf "\033[46mCyan Background\033[0m\n"
+printf "\033[47m\033[30mWhite BackgroundBlackText\033[0m\n"
+printf "\033[0m"
